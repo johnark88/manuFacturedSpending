d249f2fb689878b1f66bd889c5a2935230a7efe4
John A <johnmiltonapps@gmail.com>
2017-05-17 18:13:53 -0500

    html add

--- public/scripts/controllers/logOnController.js
+++ public/scripts/controllers/logOnController.js
@@ -1,5 +1,8 @@
 routerApp.controller('logOnController', [ '$scope', '$http', function($scope, $http){
   console.log('Log On controller');
 
-  
+  $scope.logOn = function(){
+    console.log('LogOn');
+  };
+
 }]);
