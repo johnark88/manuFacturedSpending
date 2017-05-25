b9a009cb825330833b6966317743a09fbe70ce86
johnark88 <john.arkema@gmail.com>
2017-05-25 16:36:01 -0500

    login page

--- public/scripts/controllers/logOnController.js
+++ public/scripts/controllers/logOnController.js
@@ -16,20 +16,32 @@ routerApp.controller('logOnController', ['$scope', '$http', function($scope, $ht
 
     var provider = new firebase.auth.GoogleAuthProvider();
     firebase.auth().signInWithPopup(provider).then(function(result) {
-  // This gives you a Google Access Token. You can use it to access the Google API.
-  var token = result.credential.accessToken;
-  // The signed-in user info.
-  var user = result.user;
-  // ...
-}).catch(function(error) {
-  // Handle Errors here.
-  var errorCode = error.code;
-  var errorMessage = error.message;
-  // The email of the user's account used.
-  var email = error.email;
-  // The firebase.auth.AuthCredential type that was used.
-  var credential = error.credential;
-  // ...
-});
-  };
-}]);
+      // This gives you a Google Access Token. You can use it to access the Google API.
+      var token = result.credential.accessToken;
+      // The signed-in user info.
+      var user = result.user;
+      // ...
+    }).catch(function(error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      // The email of the user's account used.
+      var email = error.email;
+      // The firebase.auth.AuthCredential type that was used.
+      var credential = error.credential;
+      // ...
+    }); //end catch
+  }; //end log on function
+
+
+  //sign user out from app
+  $scope.signOut = function() {
+    firebase.auth().signOut().then(function() {
+      // Sign-out successful.
+      console.log('You logged out');
+    }).catch(function(error) {
+      // An error happened.
+      console.log(error, 'error');
+    }); //end catch
+  }; //end signOut
+}]); //end controller
