446617e6be443f8915d3f5a3958f75c1ff396694
johnark88 <john.arkema@gmail.com>
2017-05-25 10:48:49 -0500

    moment formate

--- public/scripts/controllers/homeController.js
+++ public/scripts/controllers/homeController.js
@@ -4,16 +4,20 @@ routerApp.controller('homeController', ['$scope', '$http', function($scope, $htt
   //Get user inputs & send to server
   $scope.formSubmit = function() {
     //get input from users - in object to send to server
+      $scope.newDate = moment($scope.date).format('MMMM Do YYYY');
+
+      console.log($scope.newDate, 'NEWDATE');
     var formDataToSend = {
-      date: moment($('#datepicker').val()).format('MM-DD-YYYY'),
+      // get rid of jquery!
+      date: $scope.newDate,
       location: $scope.locationSelect, // create drop down and let user add to it
       typeSelect: $scope.typeSelect,  //
       credCardUsed: $scope.creditCardUsed, // company(citi/chase/Amex) - personal or business - program(air/cash/gas)
       totalSpent: $scope.totalSpent,
       fee: $scope.fee,
       pointsEarned: $scope.pointsEarned
-      // last four digits of gift card
-      // vin # below barcode
+      // last four digits of gift card Opt
+      // Serialnumber Opt
     }; //end object to send
     console.log(formDataToSend, 'form obj');
 
@@ -21,23 +25,24 @@ routerApp.controller('homeController', ['$scope', '$http', function($scope, $htt
       $http({
         method: 'POST',
         url: '/newGiftCard',
-        data: formDataToSend 
+        data: formDataToSend
       }).then(function(results){
         console.log(results, 'results');
       });//end http.then
         //check value of check box selected = true / else its not selected = false
         //if true change page to moneyOrder.html to add in money order details
       if ($scope.checkBoxValue === true) {
-        console.log('its true - it is selected');
+        console.log('true - it is selected');
         window.location = "/views/moneyOrder.html";
       }else{
-        console.log('its false - it is not selected');
+        console.log('false - it is not selected');
         // window.location = list of all giftcards
       }
         //call clearFirstForm
       clearFirstForm();
       }; //end formSubmit
 
+      //ng show ???
 
         //function to clear first form in home.html
       function clearFirstForm(){
