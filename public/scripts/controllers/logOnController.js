dfea121c94b04a34ed5fd78a8c54380a0326645a
johnark88 <john.arkema@gmail.com>
2017-05-25 20:11:15 -0500

    logOn

--- public/scripts/controllers/logOnController.js
+++ public/scripts/controllers/logOnController.js
@@ -1,6 +1,6 @@
 routerApp.controller('logOnController', ['$scope', '$http', function($scope, $http) {
   console.log('Log On controller');
-
+  $scope.acountDetails = {};
   // Initialize Firebase
   var config = {
     apiKey: "AIzaSyCm_yeHJa4tHtiINvTeq5FHBOynBIZFfYc",
@@ -14,14 +14,16 @@ routerApp.controller('logOnController', ['$scope', '$http', function($scope, $ht
 
   $scope.logOn = function() {
     var provider = new firebase.auth.GoogleAuthProvider();
+
     firebase.auth().signInWithPopup(provider).then(function(result) {
-      console.log(results,results);
+      provider.addScope('https://www.googleapis.com/auth/contacts.readonly');
+      // console.log(results,results);
       // This gives you a Google Access Token. You can use it to access the Google API.
       var token = result.credential.accessToken;
-      console.log(token, 'Token');
+      // console.log(token, 'Token');
       // The signed-in user info.
       var user = result.user;
-      console.log(user, 'User');
+      // console.log(user, 'User');
     }).catch(function(error) {
       // Handle Errors here.
       var errorCode = error.code;
@@ -33,15 +35,79 @@ routerApp.controller('logOnController', ['$scope', '$http', function($scope, $ht
     }); //end catch
   }; //end log on function
 
+  firebase.auth().onAuthStateChanged(function(user) {
+           if (user) {
+             // User is signed in.
+             var displayName = user.displayName;
+             var email = user.email;
+             var emailVerified = user.emailVerified;
+             var photoURL = user.photoURL;
+             var uid = user.uid;
+             var phoneNumber = user.phoneNumber;
+             var providerData = user.providerData;
+             user.getIdToken().then(function(accessToken) {
+               $scope.signInStatus = 'Signed in';
+               $scope.signIn = 'Sign out';
+               $scope.acountDetails = JSON.stringify({
+                 displayName: displayName,
+                 email: email,
+                 emailVerified: emailVerified,
+                 phoneNumber: phoneNumber,
+                 photoURL: photoURL,
+                 uid: uid,
+                 accessToken: accessToken,
+                 providerData: providerData
+               }, null, '  ');
+               console.log($scope.acountDetails, 'acountDetails');
+               sessionStorage.userAuth = accessToken;
+               console.log(user, 'firebaseUser User');
+               //store google profile info in session storage
+               sessionStorage.userGoogleId = user.uid;
+               sessionStorage.userDisplayName = user.displayName;
+               sessionStorage.userPhotoUrl = user.photoURL;
+               $scope.ifFirebaseUser(user);
+             });
+           } else {
+             // User is signed out.
+             console.log('signed out -- in logon');
+           }
+
+         }, function(error) {
+           console.log(error);
+         });
+         console.log($scope.acountDetails, 'at the end');
 
   //sign user out from app
   $scope.signOut = function() {
     firebase.auth().signOut().then(function() {
       // Sign-out successful.
       console.log('You logged out');
+      emptySessionStorage();
+      $scope.ifFirebaseUser();
     }).catch(function(error) {
       // An error happened.
       console.log(error, 'error');
     }); //end catch
   }; //end signOut
+  $scope.ifFirebaseUser = function(user) {
+    if (user) {
+              $scope.loggedIn = true;
+              $scope.loggedOut = false;
+          } else {
+              $scope.loggedIn = false;
+              $scope.loggedOut = true;
+              // $location.reload();
+          }
+      };
+//clear session storage on log out
+var emptySessionStorage = function() {
+  sessionStorage.removeItem('userProfile');
+  sessionStorage.removeItem('idToken');
+}; // end emptyLocalStorage
+
+$scope.tb = "asasdasdasdasdasda";
+
+$scope.testBatman = function(){
+  console.log('Test button on homepage');
+};
 }]); //end controller
